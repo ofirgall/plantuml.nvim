81e3367c9f913aaf8f62ee3c6d0d40debc8ab69b
Ignacio Taranto <ignacio_taranto@protonmail.com>
2024-07-02 18:48:17 -0300

    Update `plenary-nvim`
    Use `master` until a new tag is released.

--- scripts/install-plenary.sh
+++ scripts/install-plenary.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -eu
 
+plenary_dir="${HOME}/.local/share/nvim/site/pack/vendor/start/plenary.nvim"
+
+rm -rf "${plenary_dir}"
+
 git clone \
     --depth 1 \
-    --branch v0.1.4 \
     https://github.com/nvim-lua/plenary.nvim \
-    "${HOME}/.local/share/nvim/site/pack/vendor/start/plenary.nvim"
+    "${plenary_dir}"
